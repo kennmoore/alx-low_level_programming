82768b9ed70e67e86527f5db326212842bd3021c
kenmoore <kenmoore10@gmail.com>
2023-02-22 09:34:40 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - prints the string "Keneth" from a character array.
+ * main - prints the string "_putchar" from a character array.
  * Return: 0 on success.
  */
 int main(void)
 {
-	char c[] = "Kenneth";
+	char c[] = "_putchar";
 	int i;
 
 	for (i = 0; i < 7; i++)
